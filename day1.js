61580e49efe42e78d8f70462d14558a15e020bbf
wombajo <34242405+wombajo@users.noreply.github.com>
2019-02-11 11:42:26 +0100

    Update day1.js

--- day1.js
+++ day1.js
@@ -2,19 +2,19 @@ let data = document.getElementById('data').innerHTML.split('\n').map(line => par
 let a = data.reduce((acc, curr) => acc + curr)
 
 function findDuplicateFrequency(arr) {
-  let frequency = 0
-  let frequencies = new Set()
-  while (true) {
-    for (let i = 0; i < arr.length; i++) {
-      let current = frequency += arr[i]
-      if (frequencies.has(current)) {
-        return current
-      }
-      else {
-        frequencies.add(current)
-      }
+    let frequency = 0
+    let frequencies = new Set()
+    while (true) {
+        for (let i = 0; i < arr.length; i++) {
+            let current = frequency += arr[i]
+            if (frequencies.has(current)) {
+                return current
+            }
+            else {
+                frequencies.add(current)
+            }
+        }
     }
-  }
 }
 
 console.log(`A: ${a}`)
